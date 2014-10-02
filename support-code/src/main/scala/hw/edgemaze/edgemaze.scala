ad1d008776cd45bf6f668dd5688ba337c16fc103
Teddy Sudol <magesolumin@gmail.com>
2014-10-02 17:59:28 -0400

    Removed Driver test, added TestGraphs package
    - 1 small graph so far
    - :grapes:

--- support-code/src/main/scala/hw/edgemaze/edgemaze.scala
+++ support-code/src/main/scala/hw/edgemaze/edgemaze.scala
@@ -45,4 +45,40 @@ package object util {
     }
 }
 
-
+package object TestGraphs {
+    val smallGraph: EdgeGraph =
+        EdgeGraph(
+            List(0,1,2,3,4,5,6,7,8,9,10),
+            List(
+                // Node 0
+                (0, 1, 1),
+                // Node 1
+                (1, 0, 1),
+                (1, 2, 1),
+                // Node 2
+                (2, 3, 3),
+                (2, 4, 3),
+                (2, 1, 1),
+                // Node 3
+                (3, 2, 3),
+                // Node 4
+                (4, 2, 3),
+                (4, 6, 1),
+                (4, 5, 1),
+                // Node 5
+                (5, 4, 1),
+                // Node 6
+                (6, 7, 1),
+                (6, 4, 1),
+                // Node 7
+                (7, 6, 1),
+                (7, 8, 2),
+                // Node 8
+                (8, 9, 1),
+                (8, 7, 2),
+                // Node 9
+                (9, 10, 1),
+                (9, 8, 1),
+                // Node 10
+                (10, 9, 1)))
+}
