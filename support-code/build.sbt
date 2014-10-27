1a9d5e7c3f3b212890f6c3b639ad25986c879659
Arjun Guha <arjun@cs.umass.edu>
2014-10-27 15:11:13 -0400

    equality for matrices

--- support-code/build.sbt
+++ support-code/build.sbt
@@ -2,7 +2,7 @@ name := "cmpsci220"
 
 organization := "edu.umass.cs"
 
-version := "1.6"
+version := "1.7"
 
 scalaVersion := "2.11.2"
 
--- support-code/src/main/scala/hw/tictactoe/Matrix.scala
+++ support-code/src/main/scala/hw/tictactoe/Matrix.scala
@@ -61,6 +61,17 @@ class Matrix[A] private(val dim: Int, default: A, values: Map[(Int, Int), A]) {
     builder.toString
   }
 
+  override def equals(other: Any): Boolean = other match {
+    case other: Matrix[_] => {
+      other.isInstanceOf[Matrix[_]] &&
+      this.dim == other.dim &&
+      (for (i <- 0.until(dim); j <- 0.until(dim)) yield {
+        this.get(i, j) == other.get(i, j)
+       }).forall(identity)
+    }
+    case _ => false
+  }
+
 }
 
 object Matrix {
