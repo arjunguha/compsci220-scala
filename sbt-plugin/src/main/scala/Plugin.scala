28c55e12541fa3731878a430b528aecbf5b83368
Arjun Guha <arjun@cs.umass.edu>
2016-01-17 01:43:20 -0500

    more plugin

--- sbt-plugin/src/main/scala/Plugin.scala
+++ sbt-plugin/src/main/scala/Plugin.scala
@@ -1,6 +1,5 @@
 package cmpsci220.plugin
 
-
 object Plugin extends sbt.AutoPlugin {
   import org.apache.commons.io.IOUtils
   import java.io.File
@@ -12,13 +11,12 @@ object Plugin extends sbt.AutoPlugin {
 
   override def trigger = allRequirements
 
-  Tasks.directoryWarnings()
 
   object autoImport {
     lazy val submit = TaskKey[Unit]("submit")
-    lazy val submitTests = TaskKey[Unit]("submit-tests")
     lazy val checkstyle = TaskKey[Unit]("checkstyle")
     lazy val findMisplacedFiles = TaskKey[Unit]("find-misplaced-files")
+    lazy val directoryWarnings = TaskKey[Unit]("directory-warnings")
   }
 
   import autoImport._
@@ -39,21 +37,6 @@ object Plugin extends sbt.AutoPlugin {
     println("Created submission.tar.gz. Upload this file to Moodle.")
   }
 
-  def submitTestsTask(): Unit = {
-
-    val files = findFiles("src/test/scala")(fileNameHasSuffix(_, ".scala"))
-    val tgz = TgzBuilder("test-suite.tar.gz")
-    for (file <- files) {
-      tgz.add(file)
-    }
-
-    tgz.close()
-
-    println("Created test-suite.tar.gz. Upload this file to Captain Teach.")
-  }
-
-  val scalastyleBytes = resourceToByteArray("scalastyle-config.xml")
-
   override def projectSettings = super.projectSettings ++
     Seq(org.scalastyle.sbt.ScalastylePlugin.projectSettings :_*) ++
     Seq(
@@ -64,35 +47,22 @@ object Plugin extends sbt.AutoPlugin {
         "-feature",
         "-Xfatal-warnings"
       ),
-      findMisplacedFiles := Tasks.findMisplacedFiles,
-      checkstyle := {
-        import org.scalastyle.sbt.Tasks.doScalastyle
-        import org.scalastyle.sbt.ScalastylePlugin
-        val p = Files.createTempFile(Paths.get(""), "scalastyle-config-", ".xml")
-        Files.write(p, scalastyleBytes)
-        val sources = (Paths.get("src/main/scala") ::
-          Paths.get("src/test/scala") ::
-          listDir(Paths.get(""), "*.scala")).map(_.toFile)
-        doScalastyle(
-          args = Seq(),
-          config = p.toFile,
-          configUrl = None,
-          failOnError = true,
-          scalastyleSources = sources,
-          scalastyleTarget = ScalastylePlugin.scalastyleTarget.value,
-          streams = streams.value,
-          refreshHours = 0,
-          target = target.value,
-          urlCacheFile = null)
-        Files.deleteIfExists(p)
+      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test",
+      findMisplacedFiles := Tasks.findMisplacedFiles(streams.value.log),
+      directoryWarnings := Tasks.directoryWarnings(streams.value.log),
+      checkstyle := Tasks.checkstyle(
+        streams.value,
+        target.value,
+        org.scalastyle.sbt.ScalastylePlugin.scalastyleTarget.value),
+      compile := {
+        val result = (compile in Compile).value
+        checkstyle.value
+        result
       },
+      compile <<= compile.dependsOn(findMisplacedFiles, directoryWarnings),
       submit := {
         (test in Test).value
         submitTask
-      },
-      submitTests := {
-        submitTestsTask
-      },
-      compile <<= (compile in Compile).dependsOn(checkstyle, findMisplacedFiles)
+      }
     )
 }
--- sbt-plugin/src/main/scala/Tasks.scala
+++ sbt-plugin/src/main/scala/Tasks.scala
@@ -3,23 +3,27 @@ package cmpsci220.plugin
 object Tasks {
 
   import java.nio.file.{Files, Paths, Path}
+  import sbt.Logger
+  import FileUtils._
 
-  def directoryWarnings(): Unit = {
+  val scalastyleBytes = resourceToByteArray("scalastyle-config.xml")
+
+  def directoryWarnings(log: Logger): Unit = {
 
     val main = Paths.get("").resolve("src").resolve("main").resolve("scala")
     val test = Paths.get("").resolve("src").resolve("test").resolve("scala")
 
     if (!Files.isDirectory(main)) {
-      println(s"WARNING: the directory $main is missing: there is no code to run.")
+      log.warn(s"The directory $main is missing: there is no code to run.")
     }
 
     if (!Files.isDirectory(test)) {
-      println(s"WARNING: the directory $test is missing: there are no test cases.")
+      log.warn(s"The directory $test is missing: there are no test cases.")
     }
 
   }
 
-  def findMisplacedFiles(): Unit = {
+  def findMisplacedFiles(log: Logger): Unit = {
     val scalaFiles = FileUtils.findFiles(Paths.get("")) { path =>
       path.getFileName.toString.endsWith(".scala")
     }
@@ -32,13 +36,39 @@ object Tasks {
     }
 
     if (!misplacedFiles.isEmpty) {
-      println("WARNING: The following Scala files are going to be ignored:")
+      log.warn("The following Scala files are going to be ignored:")
       for (path <- misplacedFiles) {
-        println(s"  - $path")
+        log.warn(s"  - $path")
       }
-      println("All code must be in src/main/scala and all tests must be in src/test/scala.")
+      log.warn("All code must be in src/main/scala and all tests must be in src/test/scala.")
     }
+  }
 
+  def checkstyle(streams: sbt.Keys.TaskStreams,
+                 target: java.io.File,
+                 scalastyleTarget: java.io.File): Unit = {
+    import org.scalastyle.sbt.Tasks.doScalastyle
+    val p = Files.createTempFile(Paths.get(""), "scalastyle-config-", ".xml")
+    try {
+      Files.write(p, scalastyleBytes)
+      val sources = (Paths.get("src/main/scala") ::
+        Paths.get("src/test/scala") ::
+        listDir(Paths.get(""), "*.scala")).map(_.toFile)
+      doScalastyle(
+        args = Seq("q"),
+        config = p.toFile,
+        configUrl = None,
+        failOnError = true,
+        scalastyleSources = sources,
+        scalastyleTarget = scalastyleTarget,
+        streams = streams,
+        refreshHours = 0,
+        target = target,
+        urlCacheFile = null)
+    }
+    finally {
+      Files.deleteIfExists(p)
+    }
   }
 
 }
