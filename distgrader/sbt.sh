f5f6572be7c0c30b9081f49d22b839b293435c56
Arjun Guha <arjun@cs.umass.edu>
2016-02-23 22:40:30 -0500

    Logging Akka appropriately

--- distgrader/sbt.sh
+++ distgrader/sbt.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#!/bin/bash
-ARGS=$@
-
 if [[ `uname` -eq "Darwin" ]]; then
-  echo "Logging to ~/Library/Logs/grading.log"
   LOGFILE=~/Library/Logs/grading.log
 else
-  LOGFILE=rehearsal.log
+  LOGFILE=grading.log
 fi
+
+echo "Logging to ~/Library/Logs/$LOGFILE"
 
 sbt -J-Xmx4G -Dorg.slf4j.simpleLogger.defaultLogLevel=info \
   -Dorg.slf4j.simpleLogger.logFile=$LOGFILE
