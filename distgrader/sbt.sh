b408acf5cabde015831629689f2bed4fd553232d
Arjun Guha <arjun@cs.umass.edu>
2016-11-13 10:15:08 -0500

    Grading: set controller flag

--- distgrader/sbt.sh
+++ distgrader/sbt.sh
@@ -6,4 +6,5 @@ echo "Logging to $LOGFILE"
 sbt -J-Xmx4G -Dorg.slf4j.simpleLogger.defaultLogLevel=info \
   -Dorg.slf4j.simpleLogger.showDateTime=true \
   -Dorg.slf4j.simpleLogger.dateTimeFormat="H:m:s.S" \
-  -Dorg.slf4j.simpleLogger.logFile=$LOGFILE
+  -Dorg.slf4j.simpleLogger.logFile=$LOGFILE \
+  -Dgrader.controller=10.9.0.100
