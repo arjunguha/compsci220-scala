e76e9841ac9476a0772b2923399fb43a06a26946
Rachit Nigam <rachit.nigam12@gmail.com>
2018-01-04 12:19:07 +0530

    simplify json parsing assignment

--- hw/json_parsing/template/src/main/scala/Solution.scala
+++ hw/json_parsing/template/src/main/scala/Solution.scala
@@ -1,10 +1,6 @@
 import hw.json._
 import scala.util.parsing.combinator._
 
-object JSON {
-  // Define the structure of JSON as scala case classes that extend JSONLike.
-}
-
 object JSONParser extends JSONParserLike {
 
   def parseJSON(jsonString: String): JSONLike = ???
@@ -14,13 +10,5 @@ object JSONParser extends JSONParserLike {
 object JSONPrinter extends JSONPrinterLike {
 
   def printJSON(json: JSONLike): String = ???
-
-}
-
-object JSONManipulate extends JSONManipulateLike {
-
-  def getAgeArrays(jsonString: String): String = ???
-
-  def getTreeNodes(jsonString: String, nodeName: String): String = ???
 
 }
