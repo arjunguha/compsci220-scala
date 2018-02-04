2d2cdbbdcc6c041ce9befb78fc3b3dfac260aac2
Rachit Nigam <rachit.nigam12@gmail.com>
2018-02-03 19:14:21 -0500

    add wrangling object

---
+++ lib/src/main/scala/Wrangling.scala
@@ -0,0 +1,18 @@
+import hw.json.Json
+/**
+ * Object containing functions to be implemented for Data Wrangling homework.
+ */
+trait WranglingLike {
+
+  def key(json: Json, key: String): Option[Json]
+  def fromState(data: List[Json], state: String): List[Json]
+  def ratingLT(data: List[Json], rating: Int): List[Json]
+  def ratingGT(data: List[Json], rating: Int): List[Json]
+  def category(data: List[Json], category: String): List[Json]
+  def groupByState(data: List[Json]): Map[String, List[Json]]
+  def groupByCategory(data: List[Json]): Map[String, List[Json]]
+  def bestPlace(data: List[Json]): Json
+  def hasAmbience(data: List[Json], ambience: String): List[Json]
+
+}
+
